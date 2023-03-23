dd0319b06df10035ba66e59c3fd8892210420a8f
Midhumohan2001 <midhumohan0031@gmail.com>
2023-03-23 16:20:42 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import './App.css';
+
+import "./App.css";
 import { useState } from 'react';
 
+
+
+
 function App() {
-  const [toDos,setToDos]=useState([])
-  const [toDo,setToDo]=useState('')
+
+  const[toDos,setToDos]=useState([])
+  const[toDo,setToDo]=useState('')
+  
   return (
+
     <div className="app">
     <div className="mainHeading">
       <h1>ToDo List</h1>
     </div>
     <div className="subHeading">
       <br />
-      <h2>Whoop, Text Me..😜 </h2>
+      <h2>Whoop, Text Mee..😜 </h2>
     </div>
     <div className="input">
-      <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-      <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text: toDo,status:false}])} className="fas fa-plus"></i>
+       
+     <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+         
+         {/* array of value pass through  single array using spread operator */}
+
+
+      <i onClick={()=>setToDos([...toDos,{ id:Date.now(), text:toDo,status:false}])}  className="fas fa-plus"></i>
     </div>
     <div className="todos">
+
+
+      {/* disply data */}
+      
       { toDos.map((obj)=>{
-
-
-     return ( <div className="todo">
+     return( <div className="todo">
         <div className="left">
           <input onChange={(e)=>{
             console.log(e.target.checked);
             console.log(obj);
+            //  checkbox
+            // to check condition obj===todos  using map
             setToDos(toDos.filter(obj2=>{
               if(obj2.id===obj.id){
                 obj2.status=e.target.checked
@@ -34,22 +50,27 @@ function App() {
             }))
           }} value={obj.status} type="checkbox" name="" id="" />
           <p>{obj.text}</p>
+          
         </div>
         <div className="right">
           <i className="fas fa-times"></i>
         </div>
       </div>)
-})}
+       }) }
 
-{toDos.map((obj)=>{
-  if(obj.status){
-    return(<h1>{obj.text}</h1>)
-  }
-  return null
-})}
-    </div>
-  </div>
-  );
+{
+  toDos.map((obj)=>{
+    if(obj.status){
+      return(<h1>{obj.text}</h1>)
+    }
+    return null
+  })
 }
 
-export default App;
+    </div>
+  </div>
+   
+  )
+}
+
+export default App
